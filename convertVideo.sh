9b61840e7dff66574b70a808228a6fe11f4b63e1
Toroyteach <tonytoroitich@gmail.com>
2022-11-16 21:11:13 +0300

    modified the spinner to be included in other proceses

--- convertVideo.sh
+++ convertVideo.sh
@@ -3,14 +3,14 @@
 #Author: Toroyteach
 #Desc: This script converst videos to mp3 files.
 ## does so buy creating a folder in the same directory of the
-## script file and places the converted audio file inside it 
+## script file and places the converted audio file inside it
 ## retaining the name if the video( spaces will be removed).
-#Reqiurements: this script requires FFMPEG and LAME packages 
+#Reqiurements: this script requires FFMPEG and LAME packages
 ## to be installed on your system..You also need super user
 ## prvileges to 2un it
 
 ##check if script was run with su priviledge
-if [[ $(id -u) -ne 0 ]] ; then
+if [[ $(id -u) -ne 0 ]]; then
 	echo "You need Super User priviledges to run this script"
 	exit 1
 fi
@@ -19,42 +19,67 @@ fi
 FFMPEG_PACKAGE='dpkg-query -l | grep ffmpeg'
 LAME='dpkg-query -l | grep lame'
 
-if [[ -z "$FFMPEG_PACKAGE" ]] ; then
+if [[ -z "$FFMPEG_PACKAGE" ]]; then
 	echo "FFMPEG is not installed. Please instal this package to continue"
 	echo "Requesting download persission"
 
 	read -p "Would you like to download FFMPEG? " -n 1 -r
-        echo # (optional) move to a new line
-        if [[ ! $REPLY =~ ^[Yy]$ ]] ; then
-                [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-        else
-             apt install ffmpeg -y
-        fi
+	echo # (optional) move to a new line
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+	else
+		## turn the cursor back on
+		tput civis
+
+		spin &
+		SPIN_PID=$!
+
+		trap "kill -9 $SPIN_PID" $(seq 0 15)
+
+		apt install ffmpeg -y
+
+		kill -9 $SPIN_PID
+
+		## turn the cursor back on
+		tput cvvis
+	fi
 
 fi
 
-if [[ -z "$LAME" ]] ; then
+if [[ -z "$LAME" ]]; then
 	echo "LAME is not installed. Please instal this package to continue"
 	echo "exiting...."
 
 	read -p "Would you like to download LAME? " -n 1 -r
-        echo # (optional) move to a new line
-        if [[ ! $REPLY =~ ^[Yy]$ ]] ; then
-                [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-        else
-             apt install libmp3lame0 -y
-        fi
-fi
+	echo # (optional) move to a new line
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+	else
+		## turn the cursor back on
+		tput civis
 
+		spin &
+		SPIN_PID=$!
+
+		trap "kill -9 $SPIN_PID" $(seq 0 15)
+
+		apt install libmp3lame0 -y
+
+		kill -9 $SPIN_PID
+
+		## turn the cursor back on
+		tput cvvis
+	fi
+fi
 
 ##check if output music folder for mp3 exist and create or empty it
 MUSIC_OUTPUT_DIR='musicOutput'
 CURRENT_DIR='pwd'
 
-if [ ! -d "$MUSIC_OUTPUT_DIR" ] ; then
+if [ ! -d "$MUSIC_OUTPUT_DIR" ]; then
 	echo "Music output folder does not exist"
 	echo "Creating folder..."
-	
+
 	mkdir "musicOutput"
 else
 	echo "Opps there is a folder named musicOutput"
@@ -63,29 +88,28 @@ else
 	echo "About to Delete all files inside musicOutput Directory"
 	read -p "Are you sure you a have back up? " -n 1 -r
 	echo # (optional) move to a new line
-	if [[ ! $REPLY =~ ^[Yy]$ ]] ; then
-    		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 	else
 		cd "$MUSIC_OUTPUT_DIR"
 		rm -r *
 		echo "Done deleted everything"
 		cd ../
 	fi
-	
-fi
 
+fi
 
 ##start the process of conversion
 ## create a function to store spinner/proggres animation
 spin() {
-    spinner="/|\\-/|\\-"
-    while :; do
-        for i in $(seq 0 7); do
-            echo -n "${spinner:$i:1}"
-            echo -en "\010"
-            sleep 1
-        done
-    done
+	spinner="/|\\-/|\\-"
+	while :; do
+		for i in $(seq 0 7); do
+			echo -n "${spinner:$i:1}"
+			echo -en "\010"
+			sleep 1
+		done
+	done
 }
 
 ## turn the cursor off
@@ -102,17 +126,17 @@ trap "kill -9 $SPIN_PID" $(seq 0 15)
 ## run the script to convert the audio
 ## using xargs to create the arguements on the fly
 ## and output the results to output DIR
-find . -name "*.mp4" -o -name "*.mkv" -o -name "*.webm" | xargs  -d "\n"  -I {} ffmpeg -hide_banner -loglevel error -i {} -b:a 320K -vn "$MUSIC_OUTPUT_DIR"/{}.mp3
+find . -name "*.mp4" -o -name "*.mkv" -o -name "*.webm" | xargs -d "\n" -I {} ffmpeg -hide_banner -loglevel error -i {} -b:a 320K -vn "$MUSIC_OUTPUT_DIR"/{}.mp3
 
 cd "$MUSIC_OUTPUT_DIR"
 ## rename all the newly created files to remove redundant .mp4 naming
 for music_mp3_file in *; do
-    mv "$music_mp3_file" "`echo $music_mp3_file | sed  "s/.mp4//g;s/.mkv//g;s/.webm//g"`";
+	mv "$music_mp3_file" "$(echo $music_mp3_file | sed "s/.mp4//g;s/.mkv//g;s/.webm//g")"
 done
 
 echo "Finished Succesfully"
 
-## turn the cursor back on 
+## turn the cursor back on
 tput cvvis
 
 exit 0
