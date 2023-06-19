ee92104e836a2df26a4a59e3ca1f733064caaa47
Toroyteach <tonny35toro@gmail.com>
2023-06-19 19:43:18 +0300

    finished making script that converts video to mp3 files and outputs to another folder

--- convertVideo.sh
+++ convertVideo.sh
@@ -10,96 +10,11 @@
 ## prvileges to 2un it
 
 ##check if script was run with su priviledge
-if [[ $(id -u) -ne 0 ]]; then
-	echo "You need Super User priviledges to run this script"
-	exit 1
-fi
+# if [[ $(id -u) -ne 0 ]]; then
+# 	echo "You need Super User priviledges to run this script"
+# 	exit 1
+# fi
 
-##check if FFMPEG and LAME packages are installed
-FFMPEG_PACKAGE='dpkg-query -l | grep ffmpeg'
-LAME='dpkg-query -l | grep lame'
-
-if [[ -z "$FFMPEG_PACKAGE" ]]; then
-	echo "FFMPEG is not installed. Please instal this package to continue"
-	echo "Requesting download persission"
-
-	read -p "Would you like to download FFMPEG? " -n 1 -r
-	echo # (optional) move to a new line
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-	else
-		## turn the cursor back on
-		tput civis
-
-		spin &
-		SPIN_PID=$!
-
-		trap "kill -9 $SPIN_PID" $(seq 0 15)
-
-		apt install ffmpeg -y
-
-		kill -9 $SPIN_PID
-
-		## turn the cursor back on
-		tput cvvis
-	fi
-
-fi
-
-if [[ -z "$LAME" ]]; then
-	echo "LAME is not installed. Please instal this package to continue"
-	echo "exiting...."
-
-	read -p "Would you like to download LAME? " -n 1 -r
-	echo # (optional) move to a new line
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-	else
-		## turn the cursor back on
-		tput civis
-
-		spin &
-		SPIN_PID=$!
-
-		trap "kill -9 $SPIN_PID" $(seq 0 15)
-
-		apt install libmp3lame0 -y
-
-		kill -9 $SPIN_PID
-
-		## turn the cursor back on
-		tput cvvis
-	fi
-fi
-
-##check if output music folder for mp3 exist and create or empty it
-MUSIC_OUTPUT_DIR='musicOutput'
-CURRENT_DIR='pwd'
-
-if [ ! -d "$MUSIC_OUTPUT_DIR" ]; then
-	echo "Music output folder does not exist"
-	echo "Creating folder..."
-
-	mkdir "musicOutput"
-else
-	echo "Opps there is a folder named musicOutput"
-
-	## Prompt to ask user for response before deleting
-	echo "About to Delete all files inside musicOutput Directory"
-	read -p "Are you sure you a have back up? " -n 1 -r
-	echo # (optional) move to a new line
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-	else
-		cd "$MUSIC_OUTPUT_DIR"
-		rm -r *
-		echo "Done deleted everything"
-		cd ../
-	fi
-
-fi
-
-##start the process of conversion
 ## create a function to store spinner/proggres animation
 spin() {
 	spinner="/|\\-/|\\-"
@@ -111,6 +26,51 @@ spin() {
 		done
 	done
 }
+
+##check if both LAME and FFMPEG is installed
+if ! (which lame &>/dev/null && which ffmpeg &>/dev/null); then
+    echo "Either lame or ffmpeg (or both) are not installed."
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+##check if output music folder for mp3 exist and create or empty it
+MUSIC_OUTPUT_DIR='musicOutput'
+CURRENT_DIR='pwd'
+UNPROCESSED_MUSIC='unprocessedMusic'
+
+if [ ! -d "$MUSIC_OUTPUT_DIR" ]; then
+	echo "Music output folder does not exist"
+	echo "Creating folder..."
+
+	mkdir "musicOutput"
+else
+	echo "Opps there is a folder named musicOutput. Do you want to Delete all files inside the Directory"
+
+	## Prompt to ask user for response before deleting
+	read -p "Are you sure you a have back up? " -n 1 -r
+	echo # (optional) move to a new line
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+	else
+
+		cd "$MUSIC_OUTPUT_DIR"
+
+		if [ "$(ls -A)" ]; then
+			# Delete all the items within the folder
+			rm -r ./*
+			echo "Done deleting Music Output Dir Files"
+			cd ../
+		else
+
+			cd ../
+
+		fi
+
+	fi
+
+fi
+
+##start the process of conversion
 
 ## turn the cursor off
 tput civis
@@ -126,15 +86,23 @@ trap "kill -9 $SPIN_PID" $(seq 0 15)
 ## run the script to convert the audio
 ## using xargs to create the arguements on the fly
 ## and output the results to output DIR
-find . -name "*.mp4" -o -name "*.mkv" -o -name "*.webm" | xargs -d "\n" -I {} ffmpeg -hide_banner -loglevel error -i {} -b:a 320K -vn "$MUSIC_OUTPUT_DIR"/{}.mp3
+find "$UNPROCESSED_MUSIC" -type f \( -name "*.mp4" -o -name "*.mkv" -o -name "*.webm" \) -print0 | xargs -0 -I {} sh -c 'ffmpeg -hide_banner -loglevel error -i "$1" -q:a 0 -vn -c:a libmp3lame -b:a 320k "$2/$(basename "$1").mp3"' _ {} "$MUSIC_OUTPUT_DIR"
 
 cd "$MUSIC_OUTPUT_DIR"
-## rename all the newly created files to remove redundant .mp4 naming
-for music_mp3_file in *; do
-	mv "$music_mp3_file" "$(echo $music_mp3_file | sed "s/.mp4//g;s/.mkv//g;s/.webm//g")"
-done
 
-echo "Finished Succesfully"
+## rename all the newly created files to remove redundant .mp4 naming
+if [ "$(ls -A)" ]; then
+	# Delete all the items within the folder
+	for music_mp3_file in *; do
+		mv "$music_mp3_file" "$(echo "$music_mp3_file" | sed "s/.mp4//g;s/.mkv//g;s/.webm//g")"
+	done
+
+	echo "Finished Succesfully"
+else
+
+	echo "The Music Output DIR is empty. Something went wrong converting the music"
+
+fi
 
 ## turn the cursor back on
 tput cvvis
